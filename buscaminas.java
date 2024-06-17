d585ed3a49d2c1f821d1ff8197470950167bd4ff
brunoo11 <110906482+brunoo11@users.noreply.github.com>
2024-06-17 20:03:17 -0300

    Update buscaminas.java

--- buscaminas.java
+++ buscaminas.java
@@ -33,5 +33,43 @@ public class buscaminas {
 				}
 			}	
 		}
+	private static String posicion() {
+		Scanner sc = new Scanner(System.in);
+	String posicion="";
+	boolean datoValido=false;
+	
+	 while (datoValido==false) {
+         System.out.print("Ingrese la posición de fila y columna: ");
+         posicion = sc.nextLine();
+
+         // verifica que la entrada tenga el formato correcto
+         if (FormatoCorrecto(posicion)) {
+           datoValido=true; // sale del bucle si la posición es valida
+         } else {
+             System.out.println("formato incorrecto. Ingrese dos numeros separados por un espacio.");
+         }
+     }
+
+	return posicion;
+}
+	
+	private static boolean FormatoCorrecto(String posicion) {
+		boolean datoValido;
+        // verifica que la entrada tenga el formato correcto 
+        String[] partes = posicion.split(" "); //funcion encontrada en un foro
+        if (partes.length != 2) {
+            return datoValido=false;
+        }
+
+        try {
+            int fila = Integer.parseInt(partes[0]);
+            int columna = Integer.parseInt(partes[1]);//funcion buscada en internet.
+
+            // verifica que fila y columna sean números positivos
+            return fila > 0 && columna > 0;
+        } catch (NumberFormatException e) {
+            return datoValido=false; 
+        }
+    }
 	
 }
