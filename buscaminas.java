e398f19fb99b554d277362f1f218c930549d29f5
Tatincho <110904264+martinjueguen@users.noreply.github.com>
2024-06-17 16:09:50 -0300

    Update buscaminas.java

--- buscaminas.java
+++ buscaminas.java
@@ -1,10 +1,27 @@
 package Intento1;
-
+import java.util.Scanner;
 public class buscaminas {
+	
+	private static final int FILAS = 9;
+	private static final int COLUMNAS = 9;
+	private static final int MINAS = 10;
+	private static final char MINA = '*';
+	private static final char VACIO = '.';
+	private static final char NO_DESCUBIERTO = '?';
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int tableroOculto[][]=new int [FILAS][COLUMNAS];
+		char tableroVisible[][]=new char [FILAS][COLUMNAS];
+		String posicion;
+	
 	}
-
+	private static void randomizarMinas(int tableroOculto[][], String posicion) {
+		
+		
+	
+	
+	
+	
+	}
 }
